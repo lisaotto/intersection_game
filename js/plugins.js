50bef915bbdab61b2d8b46f99e7756069bb25e39
lisaotto <otto.elizabeth@gmail.com>
2015-11-07 20:31:32 -0500

    styling

--- js/plugins.js
+++ js/plugins.js
@@ -2,3 +2,17 @@ function random(obj) {
     var keys = Object.keys(obj)
     return obj[keys[ keys.length * Math.random() << 0]];
 }
+
+
+$(document).ready(function(){
+
+	$('.open').click(function(){
+	$('.rulez').fadeIn()
+});
+
+$('.close').click(function(){
+	$('.rulez').fadeOut()
+});
+
+});
+
